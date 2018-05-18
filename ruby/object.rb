cbdb01eda05dac091cc246d04f35fbbce4d19434
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-05-18 18:10:32 +0300

    Update repository structure. Add Gemfile

--- ruby/object.rb
+++ ruby/object.rb
@@ -2,6 +2,7 @@ require 'uri'
 require 'net/http'
 require 'json'
 
+# Object #yield_self
 "https://api.github.com/repos/rails/rails"
   .yield_self { |it| URI.parse(it) }
   .yield_self { |it| Net::HTTP.get(it) }
@@ -9,3 +10,10 @@ require 'json'
   .yield_self { |it| it.fetch("stargazers_count") }
   .yield_self { |it| "Rails has #{it} stargazers" }
   .yield_self { |it| puts it }
+
+
+
+# Object #method
+url = 'https://ruby-doc.org/core-2.5.0/Object.html#method-i-method'
+m = URI.method(:parse)
+puts m.call(url).host
--- ruby/object/method.rb
+++
@@ -1,5 +0,0 @@
-require 'uri'
-
-url = 'https://ruby-doc.org/core-2.5.0/Object.html#method-i-method'
-m = URI.method(:parse)
-puts m.call(url).host
